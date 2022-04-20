4f643257469763eeb57b25a637b37df44fca4ac7
Murf-y <66155122+Murf-y@users.noreply.github.com>
2022-04-20 20:01:26 +0300

    Time: 5 ms (12.14%), Space: 41.9 MB (19.24%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        if(x ==-2147483648){
+            return 0;
+        }
+        if(x/10 == 0){
+            return x;
+        }
+        
+        String a = (x<0 ? -1 * x : x)+"";
+        StringBuilder b = new StringBuilder(a);
+        long r = Long.parseLong(b.reverse().toString());
+        if(r > Integer.MAX_VALUE){
+            return 0;
+        }
+        else{
+            return x < 0 ? -1 * (int)r:(int)r;
+        }
+    }
+}
