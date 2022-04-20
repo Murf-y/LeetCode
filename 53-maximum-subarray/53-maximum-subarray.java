09b72d94540bfb69c1c7607c093ab9a55031dece
Murf-y <66155122+Murf-y@users.noreply.github.com>
2022-04-20 20:07:15 +0300

    Time: 3 ms (18.72%), Space: 73.8 MB (47.03%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {int maxSum = nums[0];
+      int sum = 0;
+      int minSum = 0;
+      for (int num : nums) {
+        sum += num;
+        maxSum = Math.max(maxSum, sum - minSum);
+        minSum = Math.min(minSum, sum);
+      }
+      return maxSum;
+    }
+}
