19679ae7be8076b0761494bf8c157af3710e932f
Murf-y <66155122+Murf-y@users.noreply.github.com>
2022-04-28 11:20:56 +0300

    Time: 25 ms (13.63%), Space: 44.1 MB (6.70%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,19 +1,34 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = 0 ; i < n; i++){
-            nums1[m + i] = nums2[i];
-        }
-        sort(nums1);
-    }
-    public void sort(int[] nums1){
-        for(int i = 1 ; i < nums1.length; i++){
-            int j = i;
-            while(j > 0 && nums1[j] < nums1[j - 1]){
-                int temp = nums1[j];
-                nums1[j] = nums1[j-1];
-                nums1[j - 1] = temp;
-                j--;
+        int[] sorted = new int[nums1.length];
+        int l = 0;
+        int r = 0;
+        int i = 0;
+        while(l < m && r < n){
+            System.out.println(" l is "+ l + " r is " + r + " i is "+ i);
+            if(nums1[l] < nums2[r]){
+                sorted[i] = nums1[l];
+                l++;
+                System.out.println("added from left");
+            }else{
+                sorted[i] = nums2[r];
+                r++;
+                System.out.println("added from right");
             }
+            i++;
+        }
+        while(l < m){
+            sorted[i] = nums1[l];
+            i++;
+            l++;
+        }
+        while(r < n){
+            sorted[i] = nums2[r];
+            i++;
+            r++;
+        }
+        for(i = 0; i < sorted.length; i++){
+            nums1[i] = sorted[i];
         }
     }
 }
