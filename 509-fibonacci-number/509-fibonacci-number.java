13649cf794a692a1e5d89ad38a2c0f5426d35fa2
Murf-y <66155122+Murf-y@users.noreply.github.com>
2022-04-28 11:42:57 +0300

    Time: 0 ms (100.00%), Space: 41.1 MB (36.03%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,13 @@
 class Solution {
     public int fib(int n) {
         if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fib(n-1) + fib(n-2);
+        int[] nums = new int[n+1];
+        nums[0] = 0;
+        nums[1] = 1;
+        for(int i = 2; i <= n; i++){
+            nums[i] = nums[i-1] + nums[i-2];
+        }
+        return nums[n];
         
         
         // 0 1 2 3 4 5 6  7  8  9 ...
