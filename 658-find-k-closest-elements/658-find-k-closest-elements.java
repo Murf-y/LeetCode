d7337df60b436cffda2200af2e456175ab507eb4
Murf-y <66155122+Murf-y@users.noreply.github.com>
2022-09-29 15:18:37 +0300

    Time: 36 ms (27.29%), Space: 68.1 MB (10.75%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,23 +1,6 @@
 class Solution {
-    public void addToSortedArray(int[] array, int i, int array_size) {
-		// Worst case: O(arrayLength)
-		
-		if(array_size == 0) array[array_size] = i;
-		else {
-			array[array_size] = i;
-			int m = array_size;
-			int j = array_size - 1;
-			while(j >= 0 && array[j] > array[m]) {
-				int temp = array[m];
-				array[m] = array[j];
-				array[j] = temp;
-				m--;
-				j--;
-			}
-		}
-	}
 	public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int[] o = new int[k];
+		int[] o = new int[k];
         int o_counter = 0;
         int res = -999;
         int l = 0;
@@ -42,7 +25,7 @@ class Solution {
         if(l >= arr.length){
             int i = arr.length - 1;
             while(k > 0){
-                addToSortedArray(o, arr[i], o_counter);
+                o[o_counter] =  arr[i];
                 o_counter++;
                 k--;
                 i--;
@@ -52,7 +35,7 @@ class Solution {
         else if(h < 0){
             int i = 0;
             while(k > 0){
-            	addToSortedArray(o, arr[i], o_counter);
+            	o[o_counter] =  arr[i];
                 o_counter++;
                 k--;
                 i++;
@@ -67,7 +50,7 @@ class Solution {
             if(res != -999){
                 i = res - 1;
                 j = res + 1;
-                addToSortedArray(o, x, o_counter);
+                o[o_counter] =  x;
                 o_counter++;
                 k--;
             }
@@ -80,12 +63,12 @@ class Solution {
             while(k > 0 && i >= 0 && j < arr.length){
                 if (Math.abs(arr[i] - x) < Math.abs(arr[j] - x) || (Math.abs(arr[i] - x) == Math.abs(arr[j] - x) && arr[i] < arr[j])){
                    
-                	addToSortedArray(o, arr[i], o_counter);
+                	o[o_counter] =  arr[i];
                     o_counter++;
                     i--;
                     k--;
                 }else{
-                	addToSortedArray(o, arr[j], o_counter);
+                	o[o_counter] =  arr[j];
                     o_counter++;
                     j++;
                     k--;
@@ -98,7 +81,7 @@ class Solution {
                 if( i < 0){
                     int w = j;
                     while(k >0 && w < arr.length){
-                    	addToSortedArray(o, arr[w], o_counter);
+                    	o[o_counter] =  arr[w];
                         o_counter++;
                         k--;
                         w++;
@@ -107,7 +90,7 @@ class Solution {
                 if( j >= arr.length){
                     int w = i;
                     while(k >0 && w >=0 ){
-                    	addToSortedArray(o, arr[w], o_counter);
+                    	o[o_counter] =  arr[w];
                         o_counter++;
                         k--;
                         w--;
@@ -115,7 +98,62 @@ class Solution {
                 }
             }
         }
-        List<Integer> a =  Arrays.stream(o).boxed().collect(Collectors.toList());
-        return a;
+        
+        o = recursiveSort(o);
+        List<Integer> list = Arrays.stream(o).boxed().toList();
+        return list;
     }
+    public int[] recursiveSort(int[] o) {
+		if(o.length <= 1) return o;
+
+		int mid = o.length/2;
+		int[] left = new int[mid];
+		int[] right = new int[o.length - mid];
+		
+		int l_i = 0;
+		int r_i = 0;
+		for (int i = 0; i < o.length; i++) {
+			if(i < mid) {
+				left[l_i] = o[i];
+				l_i++;
+			}else {
+				right[r_i] = o[i];
+				r_i++;
+			}
+		}
+		
+		merge(o, recursiveSort(left), recursiveSort(right));
+		return o;
+	}
+	public void merge(int[] arr, int[] left, int[] right) {
+		int l_i = 0;
+		int r_i = 0;
+		
+		int i;
+		for (i = 0; i < arr.length; i++) {
+			if(l_i >= left.length || r_i >= right.length) {
+				break;
+			}else {
+				if(left[l_i] < right[r_i]) {
+					arr[i] = left[l_i];
+					l_i++;
+				}else {
+					arr[i] = right[r_i];
+					r_i++;
+				}
+			}
+		}
+		
+		while(i < arr.length && l_i < left.length) {
+			arr[i] = left[l_i];
+			i++;
+			l_i++;
+		}
+		while(i < arr.length && r_i < right.length) {
+			arr[i] = right[r_i];
+			i++;
+			r_i++;
+		}
+		
+	}
 }
